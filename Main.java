1fcc1d999a7e27e54d28ff0af1cc069f13299467
Sadaqath Umair <sadaqathumairs15@gmail.com>
2022-12-22 00:10:17 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
 import java.util.*;
 public class Application {
-	private static final String tableName = "Student";
-public static void main(String[] args){
+	
+   private static final String tableName = "Student";
+	
+   public static void main(String[] args){
 
 	Column name = new Column("name", DataTypes.STRING);
 	Column age = new Column("age", DataTypes.INT);
@@ -10,19 +12,20 @@ public static void main(String[] args){
 	cols.add(name);
 	cols.add(age);
 	db.createTable(tableName, cols);
-    Map<Column, Object> map = new HashMap<>();
-    map.put(name, "Swraf");
-    map.put(age, "23");
-   db.insertTableRows(tableName, map);
-    map.clear();
-    map.put(name, "Daring");
-    map.put(age, "45");
-    db.insertTableRows(tableName, map);    
-    db.printRecords(tableName);
-    db.getTableValuesByColumnValues(tableName, age, "45");
-    db.getTableValuesByColumnValues(tableName, name, "Swraf");
-    db.dropTable(tableName);
-    db.printRecords(tableName);
+        Map<Column, Object> map = new HashMap<>();
+        map.put(name, "Swraf");
+        map.put(age, "23");
+        db.insertTableRows(tableName, map);
+        map.clear();
+        map.put(name, "Daring");
+        map.put(age, "45");
+        db.insertTableRows(tableName, map);    
+        db.printRecords(tableName);
+        db.getTableValuesByColumnValues(tableName, age, "45");
+        db.getTableValuesByColumnValues(tableName, name, "Swraf");
+        db.dropTable(tableName);
+        db.printRecords(tableName);
 	
-}
+   }
+	
 }
